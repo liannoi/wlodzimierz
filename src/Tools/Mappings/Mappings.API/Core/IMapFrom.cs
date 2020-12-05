a80ed1ab0410697d2874357814054da18e038a9a
Restyled.io <commits@restyled.io>
2020-12-05 10:59:24 +0000

    Restyled by clang-format

--- src/Tools/Mappings/Mappings.API/Core/IMapFrom.cs
+++ src/Tools/Mappings/Mappings.API/Core/IMapFrom.cs
@@ -1,12 +1,9 @@
 using AutoMapper;
 
-namespace Wlodzimierz.Tools.Mappings
-{
-public interface IMapFrom<TSource>
-{
-    void Mapping(Profile profile)
-    {
-        profile.CreateMap(typeof(TSource), GetType());
+namespace Wlodzimierz.Tools.Mappings {
+  public interface IMapFrom<TSource> {
+    void Mapping(Profile profile) {
+      profile.CreateMap(typeof(TSource), GetType());
     }
-}
+  }
 }
