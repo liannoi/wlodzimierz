8b94e7081853a3bd3c47b664055cea93ba8d9f33
Maksym Liannoi <liannoi444@gmail.com>
2021-01-17 07:11:40 +0200

    Rethinking the project through Nx and CQRS

--- src/Clients/web-spa/src/app/presentation/presentation.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './presentation.component.html',
-  styleUrls: ['./presentation.component.scss']
-})
-export class PresentationComponent {
-}
