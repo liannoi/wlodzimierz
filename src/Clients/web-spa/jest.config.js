2e09ce4c5097973b72060401a235199f23ea0387
Maksym Liannoi <liannoi444@gmail.com>
2021-01-18 10:09:39 +0200

    Modules and simple components "header" and "footer" are prepared

--- src/Clients/web-spa/jest.config.js
+++ src/Clients/web-spa/jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  projects: ['<rootDir>/apps/wlodzimierz']
+  projects: [
+    '<rootDir>/apps/wlodzimierz',
+    '<rootDir>/libs/presentation',
+    '<rootDir>/libs/infrastructure',
+    '<rootDir>/libs/domain',
+    '<rootDir>/libs/app',
+    '<rootDir>/libs/presentation/navigation'
+  ]
 };
---
+++ src/Clients/web-spa/libs/presentation/src/lib/nav-top-menu/nav-top-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wlodzimierz-nav-top-menu',
+  templateUrl: './nav-top-menu.component.html',
+  styleUrls: ['./nav-top-menu.component.scss']
+})
+export class NavTopMenuComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
